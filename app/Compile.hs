f45d7345b290bb56be5bf9364825ca457ef07fa5
Kevin Hammond <12563287+kevinhammond@users.noreply.github.com>
2021-06-11 15:01:41 +0100

    revised with correct number of arguments in datum

--- app/Compile.hs
+++ app/Compile.hs
@@ -7,6 +7,8 @@
 
 module Compile where
 
+import           Codec.Serialise
+import qualified Data.ByteString.Lazy     as BSL
 import           Data.ByteString.Short    (ShortByteString, toShort)
 import           Flat                     (flat)
 
@@ -14,11 +16,16 @@ import qualified Plutus.V1.Ledger.Scripts as P
 import qualified PlutusTx                 as P
 import qualified PlutusTx.Prelude         as P
 
-equal :: P.Data -> P.Data -> ()
-equal d1 d2 = if d1 P.== d2 then () else (P.error ())
+import           BasicValidators
 
-convert :: P.CompiledCode a -> ShortByteString
-convert  = toShort . flat . P.fromCompiledCode
+-- Cabal dependencies need to be resolved to be able to use this
+--import           Cardano.Api              (writeFileTextEnvelope)
+
+equal :: P.Data -> P.Data -> P.Data -> ()
+equal datum redeemer _ = if datum P.== redeemer then () else (P.error ())
+
+convert :: P.CompiledCode a -> BSL.ByteString
+convert  = serialise . flat . P.fromCompiledCode
 
 main :: IO ()
 main = print $ convert $ $$(P.compile [|| equal ||])
